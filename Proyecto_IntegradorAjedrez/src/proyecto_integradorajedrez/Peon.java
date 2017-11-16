edd34b34d736a8d298e02dda2ddb406510cd69d5
RickyRom <30517351+RickyRom@users.noreply.github.com>
2017-11-16 16:17:58 -0600

    avance de clases

--- Proyecto_IntegradorAjedrez/src/proyecto_integradorajedrez/Peon.java
+++ Proyecto_IntegradorAjedrez/src/proyecto_integradorajedrez/Peon.java
@@ -39,6 +39,30 @@ public class Peon extends Piezas {
     public String[][] movimiento(String[][] matriz, int x, int y, int x2, int y2) {
         if ((x >= 0 && x <= 8 && y >= 0 && y <= 8) && (x2 >= 0 && x2 <= 8 && y2 >= 0 && y2 <= 8)) {
             boolean direccion = true;
+            
+            if(direccion) {
+                if (x2 != x && y2 == y) {
+                    matriz[x2][y2] = matriz[x][y];
+                    matriz[y][x] = " ";
+                }   
+            }else if (x2 != x && y2 != y) {
+                matriz[x2][y2] = matriz[x][y];
+                matriz[x][y] = " ";
+                if (matriz[x2 + 1][y2 + 1].contains("♟") || matriz[x2 + 1][y2 + 1].contains("♞") || matriz[x2 + 1][y2 + 1].contains("♝")
+                                || matriz[x2 + 1][y2 + 1].contains("♜") || matriz[x2 + 1][y2 + 1].contains("♛") || matriz[x2 + 1][y2 + 1].contains("♚")
+                                && matriz[x2 + 2][y2 + 2].contains("♕") || matriz[x2 + 2][y2 + 2].contains("♛")) {
+                    matriz[x2 + 1][y2 + 1] = " ";
+                    System.out.println("Comiste una pieza");
+                }else if (matriz[x2 - 1][y2 - 1].contains("♟") || matriz[x2 - 1][y2 - 1].contains("♞") || matriz[x2 - 1][y2 - 1].contains("♝")
+                                || matriz[x2 - 1][y2 - 1].contains("♜") || matriz[x2 - 1][y2 - 1].contains("♛") || matriz[x2 - 1][y2 - 1].contains("♚")
+                                && matriz[x2 - 2][y2 - 2].contains("♕") || matriz[x2 - 2][y2 - 2].contains("♛")) {
+                    matriz[x2 - 1][y2 - 1] = " ";
+                    System.out.println("Comiste una pieza");
+                    return matriz;
+                }else{
+                    return matriz;
+                }
+            }
         } else {
             System.out.println("Se Salió del Rango");
         }
